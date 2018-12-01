b21f7b32b5b7e8b3df317fdc77753244c1e4fa31
André Almeida <andrefalmeida@protonmail.com>
2018-12-01 05:42:44 -0200

    Add get money and usedata model

--- server/merx/views.py
+++ server/merx/views.py
@@ -1,19 +1,41 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from merx.models import User
 import requests, json
+from hashlib import sha256
 
 @csrf_exempt
-def home(request):
-
+def login(request):
     try:
         payload = json.loads(request.body.decode('utf-8'))
         username = str(payload['username'])
         password = str(payload['password'])
+ 
+        user = User.objects.get(cpf=username)
+        pwd_hash = sha256(password.encode('utf-8')).hexdigest()
+        if user.password == pwd_hash:
+            return JsonResponse({
+                        'login' : 'true',
+                        'name' : user.name,
+                        'surname': user.surname,
+                        'money': user.money,
+                        'birthday': user.birthday
+                    })
+
     except:
        return JsonResponse({'login' : 'false'})
-    
-    if username == "user" and password == "1234":
-        return JsonResponse({'login' : 'true'})
 
-    return JsonResponse({'login' : 'false'})
+def get_money(request):
+    try:
+        payload = json.loads(request.body.decode('utf-8'))
+        username = str(payload['username'])
+
+        user = User.objects.get(cpf=username)
+
+        return JsonResponse({
+            'money' : user.money
+            })
+    except:
+        return JsonResponse({
+            'money' : -1
+            })
